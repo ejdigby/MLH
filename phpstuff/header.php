30ce5f08512b705f465d50d33a90b1ac0ed88405
Edward Digby <edward.jdigby@tiscali.co.uk>
2014-10-04 15:45:59 +0100

    Started PHP

---
+++ phpstuff/header.php
@@ -0,0 +1,16 @@
+<?php
+
+echo ''' <header>
+			<div id="HomeHub">
+				<h1>HomeHub</h1>
+			</div>
+			<div id="PageTitle">
+				<h1>Dashboard</h1>
+			</div>
+			<div id="Logout">
+				<h1><a href="">Logout</a></h1>
+			</div>
+		</header>
+		''';
+
+?>
---
+++ phpstuff/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="style.css">
+		<title>HomeHub - Dashboard</title>
+	</head>
+	<body>
+	<?php 
+		include 'header.php';
+		?>	
+
+		<div id="Wrap">
+			<div id="Content">
+				<div id="Row1">
+					<div id="Box">
+					<img src="Hive.png" />
+					<p>Heating</p>
+					</div>
+					<div id="Box">
+					<p>Lights</p>
+					</div>
+					<div id="Box">
+					<p>Alarms</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
